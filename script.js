103c98b1e5bbef1ec1978268b5ef4eea811b71f4
Manvendra Singh <manvendra_singh@gridle.io>
2019-12-05 00:58:11 +0530

    Checkbox added

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-
-    var map = AmCharts.makeChart("mapdiv", {
-      type: "map",
-      theme: "dark",
-      projection: "mercator",
-      panEventsEnabled: true,
-      backgroundColor: "#ffffff",
-      backgroundAlpha: 1,
-      zoomControl: {
-        zoomControlEnabled: false
-      },
-      dataProvider: {
-        map: "worldHigh",
-        getAreasFromMap: true,
-        areas: []
-      },
-      areasSettings: {
-        autoZoom: false,
-        color: "#E8EAF1",
-        colorSolid: "#2BB031",
-        selectedColor: "#2BB031",
-        outlineColor: "#ffffff",
-        rollOverColor: "#BFE7C1",
-        rollOverOutlineColor: "#000000"
-      }
-    });
+var map = AmCharts.makeChart("mapdiv", {
+  type: "map",
+  theme: "dark",
+  projection: "mercator",
+  panEventsEnabled: true,
+  backgroundColor: "#ffffff",
+  backgroundAlpha: 1,
+  zoomControl: {
+    zoomControlEnabled: false
+  },
+  dataProvider: {
+    map: "worldHigh",
+    getAreasFromMap: true,
+    areas: []
+  },
+  areasSettings: {
+    autoZoom: false,
+    color: "#E8EAF1",
+    colorSolid: "#2BB031",
+    selectedColor: "#2BB031",
+    outlineColor: "#ffffff",
+    rollOverColor: "#BFE7C1",
+    rollOverOutlineColor: "#BFE7C1"
+  }
+});
