743c052ca77bf5ac82ad5f01ef63fad4428b02b5
Manvendra Singh <manvendra_singh@gridle.io>
2019-12-04 21:28:37 +0530

    Map design updated

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-var map = AmCharts.makeChart("mapdiv",{
-    type: "map",
-    // theme: "light",
-    projection: "mercator",
-    panEventsEnabled : true,
-    backgroundColor : "#535364",
-    backgroundAlpha : 1,
-    zoomControl: {
-        zoomControlEnabled : false
-    },
-    dataProvider : {
-    map : "worldHigh",
-    getAreasFromMap : true,
-    areas :
-    [
-        {
-            "id": "US",
-            "showAsSelected": true
-        },
-        {
-            "id": "IN",
-            "showAsSelected": true
-        }
-    ]
-    },
-    areasSettings : {
-    autoZoom : false,
-    color : "#B4B4B7",
-    colorSolid : "#84ADE9",
-    selectedColor : "#84ADE9",
-    outlineColor : "#666666",
-    rollOverColor : "#9EC2F7",
-    rollOverOutlineColor : "#000000"
-    }
+
+    var map = AmCharts.makeChart("mapdiv", {
+      type: "map",
+      theme: "dark",
+      projection: "mercator",
+      panEventsEnabled: true,
+      backgroundColor: "#ffffff",
+      backgroundAlpha: 1,
+      zoomControl: {
+        zoomControlEnabled: false
+      },
+      dataProvider: {
+        map: "worldHigh",
+        getAreasFromMap: true,
+        areas: []
+      },
+      areasSettings: {
+        autoZoom: false,
+        color: "#E8EAF1",
+        colorSolid: "#2BB031",
+        selectedColor: "#2BB031",
+        outlineColor: "#ffffff",
+        rollOverColor: "#BFE7C1",
+        rollOverOutlineColor: "#000000"
+      }
     });
