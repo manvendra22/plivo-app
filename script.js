9cded596a6bedc25f55bd03388824979df0d3902
Manvendra Singh <mjdeora@gmail.com>
2019-12-08 11:14:03 +0530

    Map interactivity added

--- script.js
+++ script.js
@@ -11,15 +11,49 @@ var map = AmCharts.makeChart("mapdiv", {
   dataProvider: {
     map: "worldHigh",
     getAreasFromMap: true,
-    areas: []
   },
   areasSettings: {
     autoZoom: false,
     color: "#E8EAF1",
     colorSolid: "#2BB031",
+    selectable: true,
     selectedColor: "#2BB031",
     outlineColor: "#ffffff",
     rollOverColor: "#BFE7C1",
     rollOverOutlineColor: "#BFE7C1"
-  }
+  },
+  zoomOnDoubleClick: false,
+  listeners: [{
+    event: "clickMapObject",
+    method: function (e) {
+
+      // Ignore any click not on area
+      if (e.mapObject.objectType !== "MapArea")
+        return;
+
+      var area = e.mapObject;
+
+      // Toggle showAsSelected
+      area.showAsSelected = !area.showAsSelected;
+      e.chart.returnInitialColor(area);
+
+      // Update the list
+      document.getElementById("selected").innerHTML = JSON.stringify(getSelectedCountries());
+    }
+  }]
 });
+
+/**
+ * Function which extracts currently selected country list.
+ * Returns array consisting of country ISO2 codes
+ */
+function getSelectedCountries() {
+  var selected = [];
+  for (var i = 0; i < map.dataProvider.areas.length; i++) {
+    if (map.dataProvider.areas[i].showAsSelected) {
+      // selected.push(map.dataProvider.areas[i].id);
+      selected.push(map.dataProvider.areas[i].title);
+    }
+  }
+  return selected;
+}
