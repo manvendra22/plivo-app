58021f61a057e80e8e19e202179d0406ddda3ef5
Manvendra Singh <mjdeora@gmail.com>
2019-12-08 15:03:06 +0530

    Map & Tag two way bind WIP

--- script.js
+++ script.js
@@ -22,7 +22,12 @@ var map = AmCharts.makeChart("mapdiv", {
     rollOverColor: "#BFE7C1",
     rollOverOutlineColor: "#BFE7C1"
   },
+  zoomControl: {
+    zoomControlEnabled: false,
+    homeButtonEnabled: false
+  },
   zoomOnDoubleClick: false,
+  dragMap: false,
   listeners: [{
     event: "clickMapObject",
     method: function (e) {
@@ -34,18 +39,52 @@ var map = AmCharts.makeChart("mapdiv", {
       var area = e.mapObject;
 
       // Toggle showAsSelected
-      area.showAsSelected = !area.showAsSelected;
+      // area.showAsSelected = !area.showAsSelected;
+      area.showAsSelected = true;
       e.chart.returnInitialColor(area);
 
       // Update the list
-      updateList(area.title)
+      updateList(area.title, area.id)
     }
   }]
 });
 
-function updateList(area) {
-  var element = document.createElement('span')
-  element.innerHTML = area
+var selected = []
 
-  document.getElementById("selected").appendChild(element)
+function updateList(title, id) {
+
+  if (!selected.find(value => value.id === id)) {
+    selected.push({
+      id,
+      title
+    })
+    var element = document.createElement('span')
+    element.innerHTML = title
+    element.id = id
+    element.classList.add('tag')
+    element.onclick = deleteTag;
+
+    document.getElementById("selected").appendChild(element)
+  }
 }
+
+function deleteTag(e) {
+  var element = document.getElementById(e.target.id);
+  element.parentNode.removeChild(element);
+
+  // for (var i = 0; i < map.dataProvider.areas.length; i++) {
+  //   if (map.dataProvider.areas[i].showAsSelected)
+  //     selected.push(map.dataProvider.areas[i].id);
+  // }
+
+  console.log("MAP ", map)
+}
+
+// function getSelectedCountries() {
+//   var selected = [];
+//   for (var i = 0; i < map.dataProvider.areas.length; i++) {
+//     if (map.dataProvider.areas[i].showAsSelected)
+//       selected.push(map.dataProvider.areas[i].id);
+//   }
+//   return selected;
+// }
